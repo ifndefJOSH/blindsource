4ee6a5ae3fd3346c7ae7b43c6d3ccb5049c9a811
Josh Jeppson <joshua.jeppson@usu.edu>
2025-02-28 18:19:55 -0700

    getting there

--- src/blindsource.rs
+++ src/blindsource.rs
@@ -2,7 +2,8 @@ use std::fmt::format;
 
 use jack::{jack_sys::jack_default_audio_sample_t, AudioIn, AudioOut, Port};
 use nalgebra::{SVector, SMatrix};
-use ringbuf::HeapRb;
+use ringbuf::{traits::{Consumer, RingBuffer}, HeapRb};
+use itertools::*;
 
 type sample = jack_default_audio_sample_t;
 
@@ -33,7 +34,7 @@ struct Separator<const C: usize, const BufSize: usize> {
 	zeros: SMatrix<sample, C, C>,
 	covariance: SMatrix<sample, C, C>, // B_k in the matlab code
 	expectation: sample, // mu
-	audio_buffer: HeapRb<Box<[sample;BufSize]>>,
+	audio_buffer: HeapRb<Vec<SVector<sample, C>>>,
 	training_iterations: u16,
 	// Input and output ports
 	input_ports: Vec<Port<AudioIn>>,
@@ -54,24 +55,52 @@ impl<const C: usize, const BufSize: usize> Separator<C, BufSize> {
 			zeros: SMatrix::zeros(),
 			covariance: SMatrix::identity(),
 			expectation: mu,
-			audio_buffer: HeapRb::<Box<[sample;BufSize]>>::new(ring_buffer_size),
+			audio_buffer: HeapRb::<Vec<SVector<sample, C>>>::new(ring_buffer_size),
 			training_iterations: iters,
 			// Register the input ports with the client
 			input_ports: (0..C)
 				.map(|i|
-					jack_client.register_port(&format!("input_{}", i), jack::AudioIn::default()).unwrap()
+					jack_client.register_port(
+						&format!("input_{}", i),
+						jack::AudioIn::default()
+					).unwrap()
 				).collect::<Vec<_>>(),
 			// Register the output ports with the client
 			output_ports: (0..C)
 				.map(|i|
-					jack_client.register_port(&format!("input_{}", i), jack::AudioOut::default()).unwrap()
+					jack_client.register_port(
+						&format!("input_{}", i),
+						jack::AudioOut::default()
+					).unwrap()
 				).collect::<Vec<_>>(),
 		}
 	}
 
 	fn train(&mut self, ps: &jack::ProcessScope) -> jack::Control {
-		// Get the
-		// Continue
+		let training_lambda = self.density.generate_density();
+		// Get the current input and put them into the ringbuffer
+		let slices = self.input_ports.iter()
+			.map(|port| port.as_slice(ps))
+			.collect::<Vec<_>>();
+		// let mut heap_element = Box::new([0.0 as sample; C]);
+		let heap_element = (0..slices[0].len())
+			.map(|i| {
+				SVector::<sample, C>::from_vec(slices.iter()
+					.map(|slice| slice[i])
+					.collect::<Vec<_>>())
+			})
+			.collect::<Vec<_>>();
+		assert!(heap_element.len() == BufSize);
+		self.audio_buffer.push_overwrite(heap_element);
+		// Do the training
+		for frame in self.audio_buffer.iter_mut() {
+			for channeled_samples in frame.iter_mut() {
+				let g = channeled_samples.map(&training_lambda);
+				// TODO
+			}
+		}
+
+		// Continue to next frame
 		jack::Control::Continue
 	}
 }
